cef1813c421652d73344da7471f9f4b9f19af6dc
TAFH <talim2007@mail.ru>
2023-11-14 22:57:40 +0600

    Zangar Manarbek

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,25 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
+<div class="title">Dostas</div>
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
+	@keyframes sparkle {
+  		0% {
+    		transform: scale(0);
+  		}
+
+  		60% {
+    		transform: scale(1) translate(4px, 1px) rotate(8deg);
+  		}
+
+  		100% {
+    		transform: scale(0) translate(4px, 1px) rotate(8deg);
+  		}
 	}
 
-	h1 {
-		width: 100%;
-	}
+	.title {
+		font-family: 'Courier New', Courier, monospace;
+		font-size: 100px;
+		font-weight: bold;
+		color: #0A2463;
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		animation: sparkle 0.4s linear 3.5s 1 forwards;
 	}
 </style>
