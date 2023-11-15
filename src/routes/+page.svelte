aeeadc0b6b2b01e18a5811f633d7edd5eae5cae0
Zangar <zanggar.manarbek@icloud.com>
2023-11-15 10:27:12 +0600

    changed database management to be more efficient

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,42 @@
+<nav>
+	<ul class="navbar">
+		<li><a href="talimaushakhmanovich">Home</a></li>
+		<li><a href="Zangarmanarbek">About</a></li>
+		<li><a href="pilsoiivnaenbdonboenmovnq0p!">Contact</a></li>
+		<!-- <li><a href="dostas"></a></li> -->
+	</ul>
+</nav>
 <div class="title">Dostas</div>
 <style>
+
 	@keyframes sparkle {
   		0% {
-    		transform: scale(0);
+    		color: #0A2463;
   		}
-
+		20% {
+			color: white;
+		}
   		60% {
-    		transform: scale(1) translate(4px, 1px) rotate(8deg);
+    		color: chocolate;
   		}
 
-  		100% {
-    		transform: scale(0) translate(4px, 1px) rotate(8deg);
+  		80% {
+    		color: darkmagenta;
   		}
+		100% {
+			color: #0A2463;
+		}
+	}
+	.navbar {
+  		list-style-type: none;
+  		margin: 0;
+  		padding: 0;
+	}
+	.title:hover {
+		animation: sparkle 5s linear ;
+		transform: rotate(360deg);
+		transition-duration: 5s;
+		width: max-content;
 	}
 
 	.title {
@@ -20,6 +45,6 @@
 		font-weight: bold;
 		color: #0A2463;
 
-		animation: sparkle 0.4s linear 3.5s 1 forwards;
+		/* animation: sparkle 0.4s linear 3.5s 1 forwards; */
 	}
 </style>
