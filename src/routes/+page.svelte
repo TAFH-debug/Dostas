e00b12b917e1e259b95b9417972dd86eaa13e233
TAFH <talim2007@mail.ru>
2023-11-20 11:31:29 +0600

    Zangar Manarbek

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,41 @@
-<div class="title">Dostas</div>
+<script>
+  import Header from "./Header.svelte";
+
+</script>
+
+<Header/>
+<div class="body">
+	<div class="segment">
+		<img src="https://source.unsplash.com/random/200x200?sig=1" alt="None"/>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+			Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+			Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+	</div>
+</div>
 <style>
-	@keyframes sparkle {
-  		100% {
-			color: #ffffff;
-    		transform: rotate(360deg);
-  		}
+	.segment {
+		display: flex;
+		flex-direction: row;
+		color: #576460;
 	}
 
-	.title {
-		font-family: 'Courier New', Courier, monospace;
-		font-size: 100px;
-		font-weight: bold;
-		color: #0A2463;
+	.segment p {
+		text-align: center;
+		margin-left: auto;
+		margin-right: auto;
+		width: 50%;
+		font-size: 25px;
+		height: 100%;
 	}
 
-	.title:hover {
-		animation: sparkle 3s;
+	.segment img {
+		width: 40%;
+	}
+
+	.segment * {
+		padding: 10px;
 	}
 </style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,30 @@
+<nav class="header">
+    <a class="logo" href="/home">DOSTAS</a>
+    <a href="/home">Ref 1</a>
+    <a href="/about">Ref 2</a>
+    <a href="/policy">Ref 3</a>
+</nav>
+
+<style>
+    .header {
+        width: 100%;
+        background-color: #FDF0D5;
+        display: flex;
+        flex-direction: row;
+    }
+
+    .logo {
+        font-weight: bolder;
+    }
+
+    a {
+        text-decoration: none;
+        outline: none;
+        color: #576460;
+    }
+
+    .header * {
+        padding: 10px;
+        margin: 10px;
+    }
+</style>
