77cbdb42ba53f903b4412d4f1787346cf65dd239
Zangar <zanggar.manarbek@icloud.com>
2023-11-29 09:36:26 +0600

    Added Skynet and GPT-5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <Header/>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
 <div class="body">
+	<div class="bg"></div>
 	<div class="segment">
 		<h1>
 			What is <span class="app_name">Dostas</span>?
@@ -16,38 +18,66 @@
 			Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</p>
 	</div>
-	<div>
+	<div class="segment">
+		<p id="l">
+			ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+			Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+			Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
 		<h1>
-
+			What do we offer?
 		</h1>
 	</div>
+	<div class="segment" id="reg">
+		<button>
+			Register
+		</button>
+		<h1>
+			Not Convinced?
+		</h1>
+		<h2>Learn more:</h2>
+	</div>	
 </div>
 <style>
+	.bg {
+		z-index: -1;
+		height: 100%;
+		width: 100%;
+		position: fixed;
+		top: 0;
+		left: 0;
+		bottom: 100;
+		right: 0;
+		background-color: #020b0f;
+	}
 	.segment {
 		padding: 20px;
 		height: 50%;
 		display: flex;
 		flex-direction: row;
 		color: black;
+		font-family: "Poppins", sans-serif;	
 	}
 	
 	.app_name {
-		font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+		font-size: bolder;
 	}
 
 	h1 {
-  		font-size: 72px;
+  		font-size: 4vw;
   		background: -webkit-linear-gradient(0deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
   		-webkit-background-clip: text;
   		-webkit-text-fill-color: transparent;
 	}
 
 	.segment p {
+		color: aliceblue;
 		text-align: right;
 		margin-left: auto;
 		margin-right: auto;
 		width: 50%;
-		font-size: 25px;
+		font-size: 1.4vw;
 		height: 100%;
 		transform: translateY(50%);
 		border-radius: 50px;
@@ -63,5 +93,43 @@
 
 	.segment * {
 		padding: 10px;
+	}
+	#l {
+		text-align: left;
+	}
+
+
+
+	#reg {
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		color: aliceblue;
+	}
+	.segment button {
+		background-color: #000000;
+		border: solid rgba(253,29,29,1);
+		border-radius: 1vw;
+		color: aliceblue;
+		font-family: "Poppins", sans-serif;
+		padding: 30px 100px;
+		transform: translateY(100%);
+		border-radius: 2vw;
+		font-size: 2vw;
+		transition: all 0.3s ease;
+	}
+	.segment button:hover {
+		border-radius: 3vw;
+		border: solid rgba(252,176,69,1);
+		color: white;
+	}
+	#reg h1 {
+		transform: translateY(0%);
+		width: fit-content;
+		margin: 6% 0% 0%;
+	}
+	#reg h2 {
+		margin-top: 0;
+		transform: translateY(-100%);
 	}
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,6 +1,6 @@
 <nav class="header">
     <a class="logo" href="/home">DOSTAS</a>
-    <a href="/home">Ref 1</a>
+    <a href="/home">About Us</a>
     <a href="/about">Ref 2</a>
     <a href="/policy">Ref 3</a>
 </nav>
@@ -12,6 +12,7 @@
         display: flex;
         flex-direction: row;
         align-items: center;
+        border-bottom: rgba(252,176,69,1) solid thin ;
     }
 
     .logo {
@@ -21,6 +22,12 @@
         background: -webkit-linear-gradient(0deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
   		-webkit-background-clip: text;
   		-webkit-text-fill-color: transparent;
+        background-size: 200% 100%;
+        transition: background-position 1s ease-in;
+        
+    }
+    .logo:hover {
+        background-position: 100% 0;
     }
 
     a {
@@ -28,11 +35,18 @@
         outline: none;
         color: #FCB045;
         font-family: 'Courier New', Courier, monospace;
-        font-size: 25px;
+        font-size: 1.7vw;
+        transition: all 0.25s ease-in-out;
     }
 
     .header * {
         padding: 10px;
         margin: 10px;
+    }
+
+    a:hover {
+        /* transform: translateY(-20%); */
+        transform: translate(10%);
+        color: #fcc373;
     }
 </style>
