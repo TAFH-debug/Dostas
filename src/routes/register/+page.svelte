946701cec96b2c25d3c5bbd093147370788d1dc2
Zangar <zanggar.manarbek@icloud.com>
2024-04-03 21:18:00 +0500

    added registration page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,50 +1,85 @@
 <script>
   import Header from "../Header.svelte";
-
-
-  let missingRequirements = [];
-  let passwordActual, passwordConfirm, matching = true, text = "Ok.", display, color;
+  import { onMount } from 'svelte';
+  const today = new Date();
+  let passwordConfirm, validAge = true, validEmail = true, validUsername = true, matching = true, text = "Ok.", display, color, passwordError = [], usernameError = [];
   let userData = {
     email:'',
     username:'',
     password: '',
-    dob: '',
+    birthdate: '',
   }
 
+  onMount(() => {
+    const todayFormatted = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+    document.getElementById('birthdate').max = todayFormatted;
+  });
+  function emailCheck() {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    validEmail = (emailRegex.test(userData.email)) ? true : false;
+  }
+  function usernameCheck() {
+    usernameError= [];
+    const requirements = [
+      { name: 'can only have letters (a-z, A-Z) and underscores (_).', regex: /^[A-Za-z_]+$/ },
+      { name: 'can only start with letters', regex: /^[A-Za-z]/ },
+      { name: 'must between 3 and 20 characters', regex: /^.{3,20}$/ }
+    ];
+    requirements.forEach(req => {
+      if (!req.regex.test(userData.username)) {
+        usernameError.push(req.name);
+      }
+    });
+    if (validUsername) {
+      // sadasdsadsads
+      // Проверить если имя уже существует
+    }
+  }
+  function birthdateCheck() {
+    const birthdate = new Date(userData.birthdate);
+    var age = today.getFullYear() - birthdate.getFullYear();
+    var m = today.getMonth() - birthdate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
+      age--;
+    }
+    validAge = age >= 13;
+  }
   function passwordReqCheck() {
-    missingRequirements = [];
+    passwordError = [];
     const requirements = [
         { name: 'an Uppercase letter', regex: /[A-Z]/ },
         { name: 'a Lowercase letter', regex: /[a-z]/ },
         { name: 'a Digit', regex: /\d/ },
+        { name: 'no Spaces', regex: /\s/, inverse: true },
         // { name: 'a Special character', regex: /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/ },
     ];
-    requirements.forEach(req => {
-      if (!req.regex.test(passwordActual)) {
-        missingRequirements.push(req.name);
-        console.log("added " + req.name)
-      }
-    })
-    if (passwordActual.length < 8) {
-      missingRequirements.push("at least 8 characters");
-    }
-  }
-  function notMatching() {
-    if (passwordConfirm.trim() === '' || passwordConfirm !== passwordActual) {
-      matching = false;
+    if (userData.password.length >= 8) {
+      requirements.forEach(req => {
+        if (req.inverse) {
+          if (req.regex.test(userData.password)) {
+            passwordError.push(req.name);
+          }
+        } 
+        else {
+          if (!req.regex.test(userData.password)) {
+            passwordError.push(req.name);
+          }
+        }
+      })
     }
     else {
-      matching = true;
+      passwordError.push("at least 8 characters");
     }
   }
-  function validateDOB() {
-    const dob = new Date(userData.dob);
-    const today = new Date();
-    const age = Math.floor((today - dob) / (1000 * 60 * 60 * 24 * 365.25));
-    if (age > 12) {return true}
-    else {return false}
+  function passwordMatchCheck() {
+    if (passwordConfirm.trim() === '' || passwordConfirm !== userData.password) {
+      matching = false;
+    }
+    else {matching = true;}
   }
-  function send() {
+
+  // Talim/Damir, ti dolzhen sdelat eto!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  function register() {
       fetch('http://localhost:3000/api/register', {
         method: 'POST',
         headers: {
@@ -84,37 +119,47 @@
     <div class="form_cont">
       
       <div class="form_label">Email</div>
-      <input class="form_input" type="text" bind:value="{userData.email}"/>
+      <input class="form_input" type="text" bind:value="{userData.email}" on:change={emailCheck}/>
+      {#if !validEmail}
+        <div>
+          <p>Invalid email</p>
+        </div>
+      {/if}
       <div class="form_label">Username</div>
-      <input class="form_input" type="text" bind:value="{userData.username}"/>
-
+      <input class="form_input" type="text" bind:value="{userData.username}" on:change={usernameCheck}/>
+      {#if usernameError.length > 0}
+        <div>
+          <ul>
+            {#each usernameError as error}
+              <li>{error}</li>
+            {/each}
+          </ul>
+        </div>
+      {/if}
       <div class="form_label">Date of Birth</div>
-      <input class="form_input" type="date" id="dob" bind:value={userData.dob} on:change={validateDOB}>
-
+      <input class="form_input" type="date" id="birthdate" bind:value={userData.birthdate} on:change={birthdateCheck} min="1900-01-01">
       <div class="form_label">Password</div>
-      <input class="form_input" type="password"  bind:value="{passwordActual}" on:input={passwordReqCheck}/>
-      {#if missingRequirements.length > 0}
+      <input class="form_input" type="password"  bind:value="{userData.password}" on:input={passwordReqCheck}/>
+      {#if passwordError.length > 0}
         <div>
           <p>Password must have:</p>
           <ul>
-            {#each missingRequirements as requirement}
+            {#each passwordError as requirement}
               <li>{requirement}</li>
             {/each}
           </ul>
         </div>
       {/if}
       <div class="form_label">Confirm Password</div>
-      <input class="form_input" type="password" bind:value="{passwordConfirm}" on:input={notMatching}/>
+      <input class="form_input" type="password" bind:value="{passwordConfirm}" on:input={passwordMatchCheck}/>
       {#if !matching}
         <div>
           <p>Passwords do not match</p>
         </div>
       {/if}
-      <input class="form_button" type="button" value="Send" on:click={send}/>
+      <input class="form_button" type="button" value="Register" on:click={register}/> <!--pomenyayte zdes-------------------------------------------->
 
       <div class="form_status" style:display={display} style:color={color}>{text}</div>
-
-
     </div>
 
   </div>
@@ -154,7 +199,7 @@
     padding: 5px;
     margin: 5px 10%;
     border: none;
-    font-size: 150%;
+    font-size: 100%;
   }
   .form_label {
     color: white;
